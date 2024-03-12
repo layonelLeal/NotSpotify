fb25563022b4a587a2937008753a4b2ce0bfb031
layonel <layonel@LAPTOP-A6QNOGJ4>
2024-03-11 23:03:12 -0500

    Versión 1.1

---
+++ src/spotifypremium/Music.java
@@ -0,0 +1,40 @@
+package spotifypremium;
+
+/**
+ *
+ * @author layonel leal y Carol Barahona
+ */
+class Music {
+    private String nombre;
+    private String autor;
+    private int year;
+    private long reprod;
+    
+    public Music(String name, String autor, int year, long reprod){
+        this.nombre = name;
+        this.autor = autor;
+        this.year = year;
+        this.reprod = reprod;
+    }
+    
+    public void printMusic(){
+        System.out.println("Nombre: "+ nombre + " | Autor: " + autor +
+                " | Reproducciones: " + reprod + " | Anio: " + year);
+    }
+    
+    public String getNombre(){
+        return this.nombre.toLowerCase().replace(" ", "");
+    }
+    
+    public String getAutor(){
+        return this.autor.toLowerCase().replace(" ", "");
+    }
+    
+    public int getYear(){
+        return this.year;
+    }
+    
+    public long getReprod(){
+        return this.reprod;
+    }
+}
